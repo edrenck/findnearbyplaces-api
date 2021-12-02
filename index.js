d518cb399af8899f395214aae35063327f52ddb9
Edwin Renck <edrenck@gmail.com>
2021-12-01 21:37:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get("", (req, res) => {
+  res.json({ message: "API deploy successfully." });
+});
